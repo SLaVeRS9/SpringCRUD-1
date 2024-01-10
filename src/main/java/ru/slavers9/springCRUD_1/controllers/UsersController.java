c804da9f5c4a490c76c626873759f94c2359a215
SLaVeRS9 <slavers9@outlook.com>
2024-01-11 00:30:04 +0300

    Revert "SpringCRUD-2 sync"
    This reverts commit a532e7900babba3e4c4d18fa14ce6971807efd9d.

--- src/main/java/ru/slavers9/springCRUD_1/controllers/UsersController.java
+++ src/main/java/ru/slavers9/springCRUD_1/controllers/UsersController.java
@@ -1,26 +1,20 @@
-package ru.slavers9.springCRUD_2.controllers;
+package ru.slavers9.springCRUD_1.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.slavers9.springCRUD_2.models.UserModel;
-import ru.slavers9.springCRUD_2.service.UserService;
+import ru.slavers9.springCRUD_1.models.UserModel;
+import ru.slavers9.springCRUD_1.service.UserService;
 
 import javax.validation.Valid;
 
-
 @Controller
 @RequestMapping("/users")
-//@RequiredArgsConstructor
+@AllArgsConstructor
 public class UsersController {
-    private final UserService userService;
-
-    @Autowired
-    public UsersController(UserService userService) {
-        this.userService = userService;
-    }
+    private UserService userService;
 
     @GetMapping()
     public String getAllUsers(Model model) {
--- src/main/java/ru/slavers9/springCRUD_1/service/UserService.java
+++ src/main/java/ru/slavers9/springCRUD_1/service/UserService.java
@@ -1,23 +1,17 @@
-package ru.slavers9.springCRUD_2.service;
+package ru.slavers9.springCRUD_1.service;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.slavers9.springCRUD_2.models.UserModel;
-import ru.slavers9.springCRUD_2.repository.UserRepository;
+import ru.slavers9.springCRUD_1.models.UserModel;
+import ru.slavers9.springCRUD_1.repository.UserRepository;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UserService {
     UserRepository userRepository;
-
-    @Autowired
-    UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
     public List<UserModel> getAllUsers() {
         return userRepository.getAllUsers();
     }
@@ -26,7 +20,7 @@ public class UserService {
         return userRepository.getUserById(id);
     }
 
-    public void editUser(@Valid UserModel user, Long id) {
+    public void editUser(UserModel user, Long id) {
         userRepository.editUser(user, id);
     }
 
